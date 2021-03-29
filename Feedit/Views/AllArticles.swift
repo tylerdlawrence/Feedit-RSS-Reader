762ed5383266473a19940b0ef7ab32e33211f3d1
Tyler Lawrence <tyler.lawrence@hey.com>
2021-03-29 14:14:23 -0400

    added articles and smart files

---
+++ Feedit/Views/AllArticles.swift
@@ -0,0 +1,79 @@
+//
+//  AllArticles.swift
+//  Feedit
+//
+//  Created by Tyler D Lawrence on 3/17/21.
+//
+
+import Foundation
+import CoreData
+import SwiftUI
+import Combine
+import UIKit
+
+class AllArticles: NSObject, ObservableObject {
+    
+    @Published var items: [RSSItem] = [RSSItem]()
+    @Published var isOn: Bool = false
+    @Published var unreadIsOn: Bool = false
+    
+    let dataSource: RSSItemDataSource
+    var start = 0
+    
+    init(dataSource: RSSItemDataSource) {
+        self.dataSource = dataSource
+        super.init()
+    }
+    
+    func loadMore() {
+        start = items.count
+        fecthResults()
+    }
+    
+    func fecthResults() {
+        if start == 0 {
+            items.removeAll()
+        }
+        dataSource.performFetch(RSSItem.requestAllObjects(start: start))
+        if let objects = dataSource.fetchedResult.fetchedObjects {
+            items.append(contentsOf: objects)
+        }
+    }
+    
+    func fetchCount() {
+        if start == 0 {
+            //
+        }
+        dataSource.performFetch(RSSItem.requestCountAllObjects(start: start))
+        if let objects = dataSource.fetchedResult.fetchedObjects {
+            items.append(contentsOf: objects)
+        }
+    }
+    
+    func unarchive(_ item: RSSItem) {
+        let updatedItem = dataSource.readObject(item)
+        updatedItem.isArchive = false
+        updatedItem.updateTime = Date()
+        dataSource.setUpdateObject(updatedItem)
+        
+        let rs = dataSource.saveUpdateObject()
+        switch rs {
+        case .failed:
+            print("----> \(#function) failed")
+        case .saved:
+            items.removeAll { item == $0 }
+        case .unchanged:
+            print("----> \(#function) unchanged")
+        }
+    }
+    
+    func archiveOrCancel(_ item: RSSItem) {
+        let updatedItem = dataSource.readObject(item)
+        updatedItem.isArchive = !item.isArchive
+        updatedItem.updateTime = Date()
+        updatedItem.objectWillChange.send()
+        dataSource.setUpdateObject(updatedItem)
+        
+        _ = dataSource.saveUpdateObject()
+    }
+}
---
+++ Feedit/Views/RSS/SmartFeedsHomeView.swift
@@ -0,0 +1,165 @@
+//
+//  SmartFeedsHomeView.swift
+//  Feedit
+//
+//  Created by Tyler D Lawrence on 3/29/21.
+//
+
+import SwiftUI
+import CoreData
+import Combine
+
+struct SmartFeedsHomeView: View {
+
+    @StateObject var rssFeedViewModel = RSSFeedViewModel(rss: RSS(), dataSource: DataSourceService.current.rssItem)
+    @StateObject var archiveListViewModel = ArchiveListViewModel(dataSource: DataSourceService.current.rssItem)
+    @StateObject var articles: AllArticles
+    @StateObject var unread: Unread
+    
+    private var allArticlesView: some View {
+        let rss = RSS()
+        return AllArticlesView(articles: AllArticles(dataSource: DataSourceService.current.rssItem), rssFeedViewModel: RSSFeedViewModel(rss: rss, dataSource: DataSourceService.current.rssItem))
+    }
+
+    private var archiveListView: some View {
+        ArchiveListView(viewModel: ArchiveListViewModel(dataSource: DataSourceService.current.rssItem), rssFeedViewModel: self.rssFeedViewModel)
+    }
+
+    private var unreadListView: some View {
+        UnreadListView(unreads: Unread(dataSource: DataSourceService.current.rssItem))
+    }
+    
+    var body: some View {
+//        DisclosureGroup(
+//            isExpanded: $revealSmartFilters,
+//            content: {
+
+        Section(header: Text("Smart Feeds").font(.system(size: 20, weight: .medium, design: .rounded)).textCase(nil).foregroundColor(Color("text"))) {
+                HStack {
+                    ZStack{
+                        NavigationLink(destination: allArticlesView) {
+                            EmptyView()
+                        }
+                        .opacity(0.0)
+                        .buttonStyle(PlainButtonStyle())
+                    HStack{
+                        Image(systemName: "archivebox")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 21, height: 21,alignment: .center)
+                            .foregroundColor(Color.gray.opacity(0.8))
+//                            .foregroundColor(Color("tab").opacity(0.9))
+                        Text("All Articles")
+                        Spacer()
+                        Text("\(articles.items.count)")
+                            .font(.caption)
+                            .fontWeight(.bold)
+                            .padding(.horizontal, 7)
+                            .padding(.vertical, 1)
+                            .background(Color.gray.opacity(0.5))
+                            .opacity(0.4)
+                            .cornerRadius(8)
+
+                        }.accentColor(Color("tab").opacity(0.9))
+                    }
+                    .onAppear {
+                        self.articles.fecthResults()
+                    }
+                }//.listRowBackground(Color("accent"))
+
+                HStack {
+                    ZStack{
+                        NavigationLink(destination: unreadListView) {
+                            EmptyView()
+                        }
+                        .opacity(0.0)
+                        .buttonStyle(PlainButtonStyle())
+                    HStack{
+                        Image(systemName: "largecircle.fill.circle")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 21, height: 21,alignment: .center)
+                            .foregroundColor(Color("tab").opacity(0.8))
+                        Text("Unread")
+                        Spacer()
+                        Text("\(unread.items.count)")
+                            .font(.caption)
+                            .fontWeight(.bold)
+                            .padding(.horizontal, 7)
+                            .padding(.vertical, 1)
+                            .background(Color.gray.opacity(0.5))
+                            .opacity(0.4)
+                            .cornerRadius(8)
+
+                        }.accentColor(Color("tab").opacity(0.8))
+
+                    }.environment(\.managedObjectContext, Persistence.current.context)
+                    .onAppear {
+                        self.unread.fecthResults()
+                    }
+                }//.listRowBackground(Color("accent"))
+
+                HStack {
+                    ZStack{
+                    NavigationLink(destination: archiveListView) {
+                        EmptyView()
+                    }
+                    .opacity(0.0)
+                    .buttonStyle(PlainButtonStyle())
+                    HStack{
+                        Image(systemName: "star")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 21, height: 21,alignment: .center)
+//                            .foregroundColor(Color("tab").opacity(0.9))
+                            .foregroundColor(Color.yellow.opacity(0.8))
+                        Text("Starred")
+
+                            Spacer()
+                        Text("\(archiveListViewModel.items.count)")
+                            .font(.caption)
+                            .fontWeight(.bold)
+                            .padding(.horizontal, 7)
+                            .padding(.vertical, 1)
+                            .background(Color.gray.opacity(0.5))
+                            .opacity(0.4)
+                            .cornerRadius(8)
+                    }
+                }//.listRowBackground(Color("accent"))
+                    .accentColor(Color("tab").opacity(0.9))
+                    .environment(\.managedObjectContext, Persistence.current.context)
+                .onAppear {
+                    self.archiveListViewModel.fecthResults()
+                }
+            }
+//            .listRowBackground(Color("accent"))
+//            },
+//            label: {
+//                HStack {
+//                    Text("All Items")
+//                        .font(.system(size: 14, weight: .regular, design: .rounded)).textCase(.uppercase)
+//                        .frame(maxWidth: .infinity, alignment: .leading)
+//                        .contentShape(Rectangle())
+//                        .onTapGesture {
+//                            withAnimation {
+//                                self.revealSmartFilters.toggle()
+//                            }
+//                        }
+//                }
+//            })
+        }
+//            .listRowBackground(Color("darkerAccent"))
+            .accentColor(Color("tab"))
+    }
+}
+
+struct SmartFeedsHomeView_Previews: PreviewProvider {
+    static let rss = RSS()
+    static var previews: some View {
+        SmartFeedsHomeView(rssFeedViewModel: RSSFeedViewModel(rss: rss, dataSource: DataSourceService.current.rssItem), archiveListViewModel: ArchiveListViewModel(dataSource: DataSourceService.current.rssItem), articles: AllArticles(dataSource: DataSourceService.current.rssItem), unread: Unread(dataSource: DataSourceService.current.rssItem))
+        
+            .environmentObject(DataSourceService.current.rss)
+            .environmentObject(DataSourceService.current.rssItem)
+            .environment(\.managedObjectContext, Persistence.current.context)
+    }
+}
